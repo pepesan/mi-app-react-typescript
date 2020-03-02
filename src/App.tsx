84145437d3b969fa9bec73754cd293bd32519044
pepesan <pepesan@gmail.com>
2020-03-02 11:01:16 +0100

    ejemplos de formularios

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './AddTaskForm'
+import {Task} from "./task";
+import {AddTaskForm} from "./AddTaskForm";
+import {TasksList} from "./TaskList";
+interface State {
+    newTask: Task;
+    tasks: Task[];
+}
+class  App extends Component<{}, State>{
+    state = {
+        newTask: {
+            id: 1,
+            name: ""
+        },
+        tasks: []
+    };
+    render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+        return (
+            <div id="App">
+                <header>
+                    <img src={logo} alt="logo de empresa"/>
+                </header>
+                <h2>Hola App</h2>
+                <AddTaskForm
+                    task={this.state.newTask}
+                    onAdd={this.addTask}
+                    onChange={this.handleTaskChange}
+                />
+                <TasksList tasks={this.state.tasks} onDelete={this.deleteTask}/>
+            </div>
+        );
+    }
+    private addTask = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        this.setState(previousState => ({
+            newTask: {
+                id: previousState.newTask.id + 1,
+                name: ""
+            },
+            tasks: [...previousState.tasks, previousState.newTask]
+        }));
+    };
+
+    private handleTaskChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({
+            newTask: {
+                ...this.state.newTask,
+                name: event.target.value
+            }
+        });
+    };
+    private deleteTask = (taskToDelete: Task) => {
+        this.setState(previousState => ({
+            tasks: [
+                ...previousState.tasks.filter(task => task.id !== taskToDelete.id)
+            ]
+        }));
+    };
 }
 
 export default App;
